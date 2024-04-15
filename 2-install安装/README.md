377918fdf456158ede6cf862ab3aa38788ff1155
liangxiaoming <liangxm@tianyisc.com>
2024-04-15 22:51:00 +0800

    添加安装的例子

---
+++ 2-install安装/README.md
@@ -0,0 +1,8 @@
+>如何安装编译出来的二进制和头文件，库文件
+
+关键函数：  
+* install
+* add_custom_command
+
+疑问：
+为何用ninja构建器编译不过呢，它会先编译calc
---
+++ 2-install安装/calc/calc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "add.h"
+
+int main(int argc, char *argv[])
+{
+    printf("1+2=%d\n", add(1,2));
+    return 0;
+}
